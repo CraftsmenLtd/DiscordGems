9ad4930dabadc8eaaaed4e2a89a7d185179b9bfc
sakibstark11 <16sakib@gmail.com>
2023-03-31 17:43:15 +0100

    wip

--- utils/setup_bot.py
+++ utils/setup_bot.py
@@ -5,4 +5,6 @@ APP_ID = os.environ["APP_ID"]
 GUILD_ID = os.environ["GUILD_ID"]
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 
-register_command(COMMAND, APP_ID, GUILD_ID, BOT_TOKEN)
+
+if __name__ == "__main__":
+    register_command(COMMAND, APP_ID, GUILD_ID, BOT_TOKEN)
