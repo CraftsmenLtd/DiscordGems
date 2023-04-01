a989f6105c2cf4485107606196b60ac5e69d4d90
sakibstark11 <16sakib@gmail.com>
2023-04-01 13:47:41 +0100

    wip

--- utils/setup_bot.py
+++ utils/setup_bot.py
@@ -10,8 +10,10 @@ BOT_TOKEN = os.environ["BOT_TOKEN"]
 INTERACTIONS_ENDPOINT_URL = os.environ["INTERACTIONS_ENDPOINT_URL"]
 
 LOGGER = logging.getLogger()
+LOGGER.setLevel(logging.INFO)
 
 if __name__ == "__main__":
     app_registration = DiscordAppRegistration(APP_ID, BOT_TOKEN)
     LOGGER.info(app_registration.register_command(COMMAND, GUILD_ID))
-    LOGGER.info(app_registration.add_interaction_endpoint_url(INTERACTIONS_ENDPOINT_URL))
+    LOGGER.info(app_registration.add_interaction_endpoint_url(
+        INTERACTIONS_ENDPOINT_URL))
