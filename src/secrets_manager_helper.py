dc049eed6704f605581002d4c8bb56d18a69b4e1
sakibstark11 <16sakib@gmail.com>
2023-04-01 17:47:47 +0100

    caches secret

--- src/secrets_manager_helper.py
+++ src/secrets_manager_helper.py
@@ -1,5 +1,9 @@
 """Module for accessing the AWS Secrets Manager"""
+import os
+import urllib.parse
+
 import boto3
+import requests
 from mypy_boto3_secretsmanager import SecretsManagerClient
 from mypy_boto3_secretsmanager.type_defs import GetSecretValueResponseTypeDef
 
@@ -10,12 +14,34 @@ def get_secret(secrets_arn: str) -> str:
 
         :param secrets_arn: Name of the secret to get
         :type secrets_arn: str
-        :return: secret value in encoded base64
+        :return: secret value
         :rtype: str
-        """
+    """
     client: SecretsManagerClient = boto3.client(
         service_name="secretsmanager")
 
     get_secret_value_response: GetSecretValueResponseTypeDef = \
         client.get_secret_value(SecretId=secrets_arn)
     return get_secret_value_response.get("SecretString")
+
+
+def get_cached_secret(secrets_arn: str) -> str:
+    """
+        Get secret value from lambda layer
+
+        :param secrets_arn: Name of the secret to get
+        :type secrets_arn: str
+        :return: secret value
+        :rtype: str
+    """
+    headers = {
+        # this function is extremely dependent on the lambda environment
+        'X-Aws-Parameters-Secrets-Token':  os.environ['AWS_SESSION_TOKEN']
+    }
+    param_path = urllib.parse.quote(secrets_arn)
+
+    response = requests.get(
+        f'http://localhost:2773/secretsmanager/get?secretId={param_path}', headers=headers)
+    response.raise_for_status()
+
+    return response.json()["Parameter"]["Value"]
