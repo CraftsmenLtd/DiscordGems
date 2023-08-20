491008c765af6b1c7f23b96ac557403e117bc7ee
shuaibq3 <shuaib_q3@yahoo.com>
2023-08-20 21:15:39 +0600

    gems string template replaced with actual gems in discord message

--- src/message_gems_parser.py
+++ src/message_gems_parser.py
@@ -1,8 +1,9 @@
 import re
 
+gem_template_string_anchor = 'gems-'
+gem_message_string_regex = fr'(?:\s|^){gem_template_string_anchor}\d+(?:\s|$)'
+
 def get_gem_count_from_gem_string(message: str):
-    gem_template_string_anchor = 'gems-'
-    gem_message_string_regex = fr'(?:\s|^){gem_template_string_anchor}\d+(?:\s|$)'
     probable_gem_message_word = re.search(gem_message_string_regex, message)
     if not probable_gem_message_word:
         return 0
