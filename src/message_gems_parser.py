171881355dc7da103d775cce01eeb0668f14fdfe
sakibstark11 <16sakib@gmail.com>
2024-01-03 11:04:36 +0000

    refactor emoji usage

--- src/message_gems_parser.py
+++ src/message_gems_parser.py
@@ -1,19 +1,24 @@
+"""Process formatted gem message"""
 import re
+
+import emojis
 
 gem_template_string_anchor = 'gems-'
 gem_message_string_regex = fr'(?:\s|^){gem_template_string_anchor}\d+(?:\s|$)'
+
 
 def get_gem_count_from_gem_string(message: str):
     probable_gem_message_word = re.search(gem_message_string_regex, message)
     if not probable_gem_message_word:
         return 0
-    
+
     gem_message_word = probable_gem_message_word.group().strip()
     return int(gem_message_word[len(gem_template_string_anchor):])
 
+
 def get_gem_count_in_message(message: str):
-    gem_count_in_message = message.count("💎")
+    gem_count_in_message = message.count(emojis.GEM)
     if gem_count_in_message > 0:
-        return gem_count_in_message 
-    
+        return gem_count_in_message
+
     return get_gem_count_from_gem_string(message)
