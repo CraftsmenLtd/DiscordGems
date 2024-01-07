038664f281c671872425d1924b16108f0847781d
shuaibq3 <shuaib_q3@yahoo.com>
2024-01-07 12:51:54 +0600

    test names refactored in test_message_gems_decorator.py

--- test/test_message_gems_decorator.py
+++ test/test_message_gems_decorator.py
@@ -2,7 +2,7 @@ from src.message_gems_decorator import replace_gem_template_with_real_gem
 from src import emojis
 
 
-def test_message_with_gem_template_string_constructs_message_with_correct_gem_count():
+def test_should_return_4_gem_count_for_gems_4_template_string():
     undecorated_message = 'gems-4 for the help'
     gem_count = 4
     result = replace_gem_template_with_real_gem(undecorated_message, gem_count)
@@ -10,37 +10,37 @@ def test_message_with_gem_template_string_constructs_message_with_correct_gem_co
     assert result == expected
 
 
-# def test_message_with_gem_template_string_constructs_message_with_zero_gem_count():
-#     undecorated_message = 'gems-0 for the help'
-#     gem_count = 0
-#     result = replace_gem_template_with_real_gem(undecorated_message, gem_count)
-#     expected = 'for the help'
-#     assert result == expected
+def test_should_return_0_gem_count_for_gems_0_template_string():
+    undecorated_message = 'gems-0 for the help'
+    gem_count = 0
+    result = replace_gem_template_with_real_gem(undecorated_message, gem_count)
+    expected = 'gems-0 for the help'
+    assert result == expected
 
 
-# def test_message_with_multiple_gem_template_string_constructs_message_with_first_converted_to_gem():
-#     undecorated_message = f'{emojis.GEM*4} gems-3 for the help'
-#     result = replace_gem_template_with_real_gem(undecorated_message, 4)
-#     expected = 'gems-4 gems-3 for the help'
-#     assert result == expected
+def test_should_return_gem_count_from_first_template_string_for_multiple_template_strings():
+    undecorated_message = 'gems-4 gems-3 for the help'
+    result = replace_gem_template_with_real_gem(undecorated_message, 4)
+    expected = f'{emojis.GEM*4} gems-3 for the help'
+    assert result == expected
 
 
-# def test_message_with_gem_template_string_and_gem_remains_unchanged():
-#     undecorated_message = f'{emojis.GEM*3} gems-4 for the help'
-#     result = replace_gem_template_with_real_gem(undecorated_message, 3)
-#     expected = f'{emojis.GEM*3} gems-4 for the help'
-#     assert result == expected
+def test_should_return_unchanged_message_for_gem_emoji_and_template_string():
+    undecorated_message = f'{emojis.GEM*3} gems-4 for the help'
+    result = replace_gem_template_with_real_gem(undecorated_message, 3)
+    expected = f'{emojis.GEM*3} gems-4 for the help'
+    assert result == expected
 
 
-# def test_message_with_gem_template_string_in_middle_constructs_message_with_correct_gem_count():
-#     undecorated_message = f'giving {emojis.GEM*3} to you for the help'
-#     result = replace_gem_template_with_real_gem(undecorated_message, 3)
-#     expected = 'giving gems-3 to you for the help'
-#     assert result == expected
+def test_should_return_correct_gem_count_for_template_string_in_middle():
+    undecorated_message = 'giving gems-3 to you for the help'
+    result = replace_gem_template_with_real_gem(undecorated_message, 3)
+    expected = f'giving {emojis.GEM*3} to you for the help'
+    assert result == expected
 
 
-# def test_message_with_invalid_gem_template_string_remains_unchanged():
-#     undecorated_message = 'gems-1234abcd for the help'
-#     result = replace_gem_template_with_real_gem(undecorated_message, 0)
-#     expected = 'gems-1234abcd for the help'
-#     assert result == expected
+def test_should_return_unchanged_message_for_invalid_template_string():
+    undecorated_message = 'gems-1234abcd for the help'
+    result = replace_gem_template_with_real_gem(undecorated_message, 0)
+    expected = 'gems-1234abcd for the help'
+    assert result == expected
