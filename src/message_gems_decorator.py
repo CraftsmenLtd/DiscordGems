4dbbf75bbdf84844ba4db5fccfad923c03964731
shuaibq3 <shuaib_q3@yahoo.com>
2024-01-07 13:32:46 +0600

    failing test fixed it had been due to GEM emoji length increased to 5 from 1

--- src/message_gems_decorator.py
+++ src/message_gems_decorator.py
@@ -12,13 +12,13 @@ def replace_gem_template_with_real_gem(message_with_gem_template: str, gem_count
     gem_substituted_message = re.sub(
         gem_message_string_regex, emojis.GEM * gem_count, message_with_gem_template)
 
-    if gem_substituted_message[0] != emojis.GEM:
+    if gem_substituted_message[0:len(emojis.GEM)] != emojis.GEM:
         gem_substituted_message = gem_substituted_message.replace(
             emojis.GEM, f' {emojis.GEM}', 1)
 
     gem_last_index = gem_substituted_message.rfind(emojis.GEM)
-    if gem_last_index != -1 and gem_last_index != len(gem_substituted_message) - 1:
+    if gem_last_index != -1 and gem_last_index != len(gem_substituted_message) - len(emojis.GEM):
         gem_substituted_message = gem_substituted_message[:gem_last_index] + \
-            f'{emojis.GEM} ' + gem_substituted_message[gem_last_index + 1:]
+            f'{emojis.GEM} ' + gem_substituted_message[gem_last_index + len(emojis.GEM):]
 
     return gem_substituted_message
